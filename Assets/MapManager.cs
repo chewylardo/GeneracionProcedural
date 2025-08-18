33a8cd5f55016204c2e04950819bda29d8dcd50a
chewylardo <127953611+chewylardo@users.noreply.github.com>
2025-08-18 17:55:06 -0400

    se crea un mapa de 40 * 40

---
+++ Assets/MapManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public GameObject PisoPrefab;
+    public GameObject ParedPrefab; 
+    void Start()
+    {
+        CrearMap();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+    int[,] mapa = new int[40, 40];
+    
+
+    public float tamañoCelda = 1f;
+    void CrearMap()
+    { 
+
+     //por ahora se esta creando con puros suelos por que la biparticion no esta hecha
+     for (int y = 0; y < mapa.GetLength(0); y++){
+         for (int x = 0; x < mapa.GetLength(1); x++){
+                Vector3 position = new Vector3(x * tamañoCelda, -y * tamañoCelda, 0); 
+                GameObject toInstantiate = null;
+
+                if (mapa[y, x] == 0){
+                    toInstantiate = PisoPrefab;
+                }
+                else if (mapa[y, x] == 1){
+                    toInstantiate = ParedPrefab;
+                }
+
+                if (toInstantiate != null)
+                    Instantiate(toInstantiate, position, Quaternion.identity, this.transform);
+                
+         }
+        }
+    }
+}
+
