093616c9680be4fdd4e22a9ea4dfd688e1335bc1
chewylardo <127953611+chewylardo@users.noreply.github.com>
2025-08-18 20:51:07 -0400

    codigo

--- Assets/DrunkenAgent.cs
+++ Assets/DrunkenAgent.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DivisionBinaria : MonoBehaviour
-{
-    // Start is called before the first frame update
+{   
+    //probabilidad de cambiar de direccion y probabilidad de crear sala
+    public int Pc = 5;
+    public int Pr = 5;
+
+
+     
     void Start()
     {
         
